8f9f267694c4fa03c331892989e0097543adfac7
aadil124 <ansariaadil124@gmail.com>
2024-02-16 12:27:34 +0530

    implement auth for sign up

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,8 +4,13 @@ import Login from "./Login";
 import Browse from "./Browse";
 import ForgotPassword from "./ForgotPassword";
 import ErrorPage from "./ErrorPage";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../Redux/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -23,9 +28,34 @@ const Body = () => {
       path: "/error",
       element: <ErrorPage />,
     },
-
-    useEffect(() => {}, []),
   ]);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const { uid, email, displayName, photoURL } = user;
+        console.log(uid, email, displayName);
+
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        dispatch(removeUser());
+      }
+    });
+  }, [auth]);
+
   return (
     <div>
       <RouterProvider router={appRouter} />
