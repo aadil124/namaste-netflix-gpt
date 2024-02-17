a0fad47bb5d42ba5274febfca76ee6cb2a9609a1
aadil124 <ansariaadil124@gmail.com>
2024-02-17 22:11:52 +0530

    sign out function added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  console.log(user);
 
-  const handleSignOut = () => {};
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+      })
+      .catch((error) => {
+        // An error happened.
+        navigate("/error");
+      });
+  };
 
   return (
     <div className="absolute bg-gradient-to-b from-black z-50 w-screen flex justify-between">
@@ -11,15 +28,21 @@ const Header = () => {
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
-      <div className="m-2 p-4 flex">
-        <img className="mt-4" src="https://github.com/account" alt="userIcon" />
-        <button
-          className="bg-red-600 rounded-md px-2 text-white font-bold "
-          onClick={handleSignOut}
-        >
-          Sign out
-        </button>
-      </div>
+      {user && (
+        <div className="m-2 p-4 flex">
+          <img
+            className="mt-4"
+            src="https://github.com/account"
+            alt="userIcon"
+          />
+          <button
+            className="bg-red-600 rounded-md px-2 text-white font-bold "
+            onClick={handleSignOut}
+          >
+            Sign out
+          </button>
+        </div>
+      )}
     </div>
   );
 };
