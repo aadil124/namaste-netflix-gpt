d8c47e88881a0000be0f686dc885e7f96006994d
aadil124 <ansariaadil124@gmail.com>
2024-02-20 20:18:33 +0530

    unsubscribe AuthStateChange

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,30 +23,34 @@ const Header = () => {
   };
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/auth.user
 
-        const { uid, email, displayName, photoURL } = user;
-        console.log(uid, email, displayName);
-        dispatch(
-          addUser({
-            uid: uid,
-            email: email,
-            displayName: displayName,
-            photoURL: photoURL,
-          })
-        );
-        navigate("/browse");
-        // ...
-      } else {
-        // User is signed out
-        // ...
-        dispatch(removeUser());
-        navigate("/");
-      }
-    });
+  const unsubscribe = onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
+
+      const { uid, email, displayName, photoURL } = user;
+      console.log(uid, email, displayName);
+      dispatch(
+        addUser({
+          uid: uid,
+          email: email,
+          displayName: displayName,
+          photoURL: photoURL,
+        })
+      );
+      navigate("/browse");
+      // ...
+    } else {
+      // User is signed out
+      // ...
+      dispatch(removeUser());
+      navigate("/");
+    }
+  });
+
+  // this will be called when my component unmounts and this will be unsubscribe my onAuthStateChanged.
+  return () => unsubscribe(); 
   }, []);
 
   return (
