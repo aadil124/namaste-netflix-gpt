52f018b13cf1b42522e6fc42f13fd61bf6f7069c
aadil124 <ansariaadil124@gmail.com>
2024-03-01 11:21:29 +0530

    update login

--- src/components/Login.js
+++ src/components/Login.js
@@ -219,7 +219,7 @@ const Login = () => {
                 The information collected by Google reCAPTCHA is subject to the
                 Google Privacy Policy and Terms of Service, and is used for
                 providing, maintaining, and improving the reCAPTCHA service and
-                for general security purposes (it is not used for personalised
+                for general security purposes (it is not used for personalized
                 advertising by Google).
                 <span
                   onClick={handleLearnMore}
