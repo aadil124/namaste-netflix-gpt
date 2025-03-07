561cc7b604db25a057982578fd43c80b34fb133e
aadil124 <ansariaadil124@gmail.com>
2025-03-07 23:40:34 +0530

    added api options

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,13 @@ export const USER_AVATAR =
 
 export const BACKGROUND_IMAGE =
   "https://assets.nflxext.com/ffe/siteui/vlv3/32c47234-8398-4a4f-a6b5-6803881d38bf/eed3a573-8db7-47ca-a2ce-b511e0350439/IN-en-20240122-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+
+  export const API_OPTIONS = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzBiNzE4YTgzMzcwYzIwNDE4ZjYyZjhmMjFmZDk1MyIsIm5iZiI6MTc0MDk5MTkzOS44ODcsInN1YiI6IjY3YzU2ZGMzNDhlZTkwMTVhYjdhN2RlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.I8jQmJr_1hSIKvqkZ5us354QkDw6oFo2zUNGrquIqRQ",
+    },
+  };
