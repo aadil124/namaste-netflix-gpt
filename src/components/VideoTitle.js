5aeeb29db914a95f100c41cec6aa65b7fb45d53e
aadil124 <ansariaadil124@gmail.com>
2025-03-17 17:13:29 +0530

    added css

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const VideoTitle = ({ movieTitle, movieOverview }) => {
   return (
-    <div className="pt-36 px-10">
+    <div className="pt-72 px-10 absolute text-white">
       <h1 className="text-4xl font-bold">{movieTitle}</h1>
       <p className="w-1/4 py-2">{movieOverview}</p>
-      <button className="px-5 py-2 bg-slate-500 text-white rounded-md">
+      <button className="px-5 py-2 bg-white text-black rounded-md hover:opacity-70">
         Play
       </button>
-      <button className="mx-3 px-5 py-2 bg-slate-500 text-white rounded-md opacity-50">
+      <button className="mx-3 px-5 py-2 bg-white text-black rounded-md opacity-50 hover:opacity-40">
         Help
       </button>
     </div>
