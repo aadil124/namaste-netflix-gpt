81cc7057468e32c82362fdf7d8475209721eb278
aadil124 <ansariaadil124@gmail.com>
2025-03-19 17:56:17 +0530

    added movie slice

--- src/Redux/appStore.js
+++ src/Redux/appStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSliceReducer from "./userSlice";
+import moviesSliceReducer from "./moviesSlice";
 
 const appStore = configureStore({
   reducer: {
     user: userSliceReducer,
+    movies: moviesSliceReducer,
   },
 });
 
