89676a247ca217fe23f176e11b3df568793a1d34
aadil124 <ansariaadil124@gmail.com>
2025-04-10 16:34:01 +0530

    edit

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -6,7 +6,7 @@ const VideoTitle = ({ movieTitle, movieOverview }) => {
       <h1 className="text-4xl font-bold">{movieTitle}</h1>
       <p className="w-1/4 py-2">{movieOverview}</p>
       <button className="px-5 py-2 bg-white text-black rounded-md hover:opacity-70">
-        Play
+        Play button
       </button>
       <button className="mx-3 px-5 py-2 bg-white text-black rounded-md opacity-50 hover:opacity-40">
         Help
