de8405a29f1cce4a0e5ecb55015a5e5dcd9db278
aadil124 <ansariaadil124@gmail.com>
2025-08-19 00:09:07 +0530

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 // export const LOGO =
 // "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
-export const USER_AVATAR =
-  "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png";
+// export const USER_AVATAR =
+//   "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png";
 
 export const BACKGROUND_IMAGE =
   "https://assets.nflxext.com/ffe/siteui/vlv3/32c47234-8398-4a4f-a6b5-6803881d38bf/eed3a573-8db7-47ca-a2ce-b511e0350439/IN-en-20240122-popsignuptwoweeks-perspective_alpha_website_large.jpg";
