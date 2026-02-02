c36775e0b8e082c37d1f41044006aa8be7f5cee0
Mohd Adil Ansari <98684212+aadil124@users.noreply.github.com>
2026-02-02 12:59:52 +0530

    Update userSlice.js

--- src/Redux/userSlice.js
+++ src/Redux/userSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-  name: "user",
+  name: "users",
   initialState: null,
   reducers: {
     addUser: (state, action) => {
